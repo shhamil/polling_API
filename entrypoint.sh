2f1b83435afa1bb90730864d2bceff4cfc247b0a
shhamil <kurban0595@inbox.ru>
2021-12-13 10:29:20 +0300

    some fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+
 echo "from django.contrib.auth.models import User; User.objects.create_superuser(username='admin', email='admin@example.com', password='admin')" | python manage.py shell
 exec gunicorn config.wsgi:application -b 0.0.0.0:8000 --reload
